d1786454ad26aa90bf2a3fc34c88200b55f61ef9
0xCourtney <23440493+0xCourtney@users.noreply.github.com>
2023-11-06 15:45:42 -0500

    feat: export metadata types

--- deployment/index.ts
+++ deployment/index.ts
@@ -3,6 +3,7 @@ import {ContractDeploymentMetadata, DeploymentMetadata} from './types';
 import arbitrumMetadata from './arbitrum.json';
 import arbitrumGoerliMetadata from './arbitrumGoerli.json';
 import arbitrumNovaMetadata from './arbitrumNova.json';
+export { ContractDeploymentMetadata, ContractKey, ContractType,  DeploymentMetadata } from './types';
 export const arbitrum = arbitrumMetadata as DeploymentMetadata;
 export const arbitrumGoerli = arbitrumGoerliMetadata as DeploymentMetadata;
 export const arbitrumNova = arbitrumNovaMetadata as { core: { [key: string]: ContractDeploymentMetadata } };
